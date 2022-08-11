5bfcd16875e3f09640843ae531f81e45e94eaffb
AgustindelaVega <agustin.delavega@ing.austral.edu.ar>
2022-08-10 22:08:07 -0300

    rgb only vegetation index fix

--- services/IndexService.py
+++ services/IndexService.py
@@ -10,12 +10,18 @@ orthophoto_path = '/multispectral/odm_orthophoto/odm_orthophoto'
 rgb_orthophoto_path = '/rgb/odm_orthophoto/odm_orthophoto'
 
 
-def check_images_missing(index_formula, red, nir, reg, gre, blue):
+def check_images_missing(index_formula, red, nir, reg, gre, blue, red_alternative, gre_alternative, blue_alternative):
     red_missing = "red" in index_formula and red is None
     nir_missing = "nir" in index_formula and nir is None
     reg_missing = "reg" in index_formula and reg is None
     gre_missing = "gre" in index_formula and gre is None
     blue_missing = "blue" in index_formula and blue is None
+
+    index_rgb = only_rgb_index(index_formula)
+    rgb_missing = index_rgb and red_alternative is None and gre_alternative is None and blue_alternative is None
+
+    if index_rgb:
+        return rgb_missing
     return red_missing or nir_missing or reg_missing or gre_missing or blue_missing
 
 
@@ -29,26 +35,49 @@ def calculate_index(project_path, indexes, custom_indexes, fig, ax):
     blue_path = project_path + orthophoto_path + '_BLU.tif'  # TODO check blue file
     blue_alternative_path = project_path + rgb_orthophoto_path + '.tif'
 
-    rgb_present_in_multispectral = check_rgb_images([red_path, gre_path, blue_path])
+    any_all_rgb_index = list(map(only_rgb_index, map(lambda x: Index[x].value, indexes))) + list(map(only_rgb_index, map(lambda x: x['formula'], custom_indexes)))
+    should_read_rgb = any_all_rgb_index.count(True) > 0
+    should_read_multispectral = any_all_rgb_index.count(False) > 0
+    red, nir, reg, gre, blue = None, None, None, None, None
+    check = None
 
-    red_present, red_img, is_red_alternative = read_img(red_path, red_alternative_path, not rgb_present_in_multispectral)
-    nir_present, nir_img, is_nir_alternative = read_img(nir_path)
-    reg_present, reg_img, is_reg_alternative = read_img(reg_path)
-    gre_present, gre_img, is_gre_alternative = read_img(gre_path, gre_alternative_path, not rgb_present_in_multispectral)
-    blue_present, blue_img, is_blue_alternative = read_img(blue_path, blue_alternative_path, not rgb_present_in_multispectral)
+    red_alternative, gre_alternative, blue_alternative = None, None, None
+    alternative_check = None
 
-    red = convert_to_array(red_img, red_present, 2 if is_red_alternative else 0)
-    nir = convert_to_array(nir_img, nir_present)
-    reg = convert_to_array(reg_img, reg_present)
-    gre = convert_to_array(gre_img, gre_present, 1 if is_gre_alternative else 0)
-    blue = convert_to_array(blue_img, blue_present, 0 if is_blue_alternative else 0)
+    if should_read_rgb:
+        print("reading rgb images")
+        red_alternative_present, red_alternative_img = read_img(red_alternative_path)
+        gre_alternative_present, gre_alternative_img = read_img(gre_alternative_path)
+        blue_alternative_present, blue_alternative_img = read_img(blue_alternative_path)
 
-    check = np.logical_and.reduce(get_check(red, nir, reg, gre, blue))
+        red_alternative = convert_to_array(red_alternative_img, red_alternative_present, 2)
+        gre_alternative = convert_to_array(gre_alternative_img, gre_alternative_present, 1)
+        blue_alternative = convert_to_array(blue_alternative_img, blue_alternative_present, 0)
+
+        alternative_check = np.logical_and.reduce(get_check(red_alternative, None, None, gre_alternative, blue_alternative))
+
+    if should_read_multispectral:
+        print("reading multispectral images")
+        red_present, red_img = read_img(red_path)
+        nir_present, nir_img = read_img(nir_path)
+        reg_present, reg_img = read_img(reg_path)
+        gre_present, gre_img = read_img(gre_path)
+        blue_present, blue_img = read_img(blue_path)
+
+        red = convert_to_array(red_img, red_present)
+        nir = convert_to_array(nir_img, nir_present)
+        reg = convert_to_array(reg_img, reg_present)
+        gre = convert_to_array(gre_img, gre_present)
+        blue = convert_to_array(blue_img, blue_present)
+
+        check = np.logical_and.reduce(get_check(red, nir, reg, gre, blue))
+
     result = {}
     paths = {}
     for index in indexes:
-        if not check_images_missing(Index[index].value, red, nir, reg, gre, blue):
-            val = get_index(index, check, red, nir, reg, gre, blue)
+        if not check_images_missing(Index[index].value, red, nir, reg, gre, blue, red_alternative, gre_alternative, blue_alternative):
+            val = get_index(index, check, red, nir, reg, gre, blue) if not only_rgb_index(Index[index].value) \
+                else get_index(index, alternative_check, red_alternative, nir, reg, gre_alternative, blue_alternative)
             result[index] = val
             paths[index] = {
                 'img': project_path + '/index_' + index + '.png',
@@ -59,8 +88,9 @@ def calculate_index(project_path, indexes, custom_indexes, fig, ax):
             paths[index] = None
 
     for custom_index in custom_indexes:
-        if not check_images_missing(custom_index['formula'], red, nir, reg, gre, blue):
-            val = get_custom_index(custom_index['formula'], check, red, nir, reg, gre, blue)
+        if not check_images_missing(custom_index['formula'], red, nir, reg, gre, blue, red_alternative, gre_alternative, blue_alternative):
+            val = get_custom_index(custom_index['formula'], check, red, nir, reg, gre, blue) if not only_rgb_index(custom_index['formula']) \
+                else get_custom_index(custom_index['formula'], alternative_check, red_alternative, nir, reg, gre_alternative, blue_alternative)
             result[custom_index['name']] = val
             paths[custom_index['name']] = {
                     'img': project_path + '/index_' + custom_index['name'] + '.png',
@@ -75,7 +105,7 @@ def calculate_index(project_path, indexes, custom_indexes, fig, ax):
             create_heatmap(result[key],
                            project_path + '/index_' + key + '.png',
                            True,
-                           4000,
+                           3000,
                            fig,
                            ax)
             numpy.save(project_path + '/index_' + key + '.npy', result[key])
@@ -90,12 +120,10 @@ def check_rgb_images(paths):
     return all_rgb_present_in_multispectral
 
 
-def read_img(img, alternative_path=None, skip_alternative=False):
+def read_img(img):
     if img is not None and os.path.exists(img):
-        return True, cv2.imread(img, cv2.IMREAD_UNCHANGED), False
-    if alternative_path is not None and os.path.exists(alternative_path) and not skip_alternative:
-        return True, cv2.imread(alternative_path, cv2.IMREAD_UNCHANGED), True
-    return False, None, False
+        return True, cv2.imread(img, cv2.IMREAD_UNCHANGED)
+    return False, None
 
 
 def convert_to_array(img, present, index=0):
@@ -149,5 +177,9 @@ def get_zone_above_threshold(src, threshold_max, threshold_min, fig, ax):
     array[array >= threshold_max] = 0
     array[array <= threshold_min] = 0
     array[(array > threshold_min) & (array < threshold_max)] = 1
-    create_heatmap(array, out, True, 4000, fig, ax, min_val=1, max_val=1)
+    create_heatmap(array, out, True, 3000, fig, ax, min_val=1, max_val=1)
     return out
+
+
+def only_rgb_index(index):
+    return ("red" in index or "gre" in index or "blue" in index) and "nir" not in index and "reg" not in index
